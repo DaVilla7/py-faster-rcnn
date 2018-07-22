afcb6558b1b64927818b10c4ec3a734405d67a31
DaVilla7 <773954470@qq.com>
2018-07-22 09:54:59 +0800

    Update bbox_transform.py

--- lib/fast_rcnn/bbox_transform.py
+++ lib/fast_rcnn/bbox_transform.py
@@ -18,6 +18,8 @@ def bbox_transform(ex_rois, gt_rois):
     gt_ctr_x = gt_rois[:, 0] + 0.5 * gt_widths
     gt_ctr_y = gt_rois[:, 1] + 0.5 * gt_heights
 
+    #对中心点和宽和高求解梯度
+    #求解中心点梯度用坐标之差处以估计的bbox的宽度，纵坐标处以高度
     targets_dx = (gt_ctr_x - ex_ctr_x) / ex_widths
     targets_dy = (gt_ctr_y - ex_ctr_y) / ex_heights
     targets_dw = np.log(gt_widths / ex_widths)
@@ -28,26 +30,37 @@ def bbox_transform(ex_rois, gt_rois):
     return targets
 
 def bbox_transform_inv(boxes, deltas):
+    #通过bbox转换将anchors转换为proposals
+    #anchor是预设值，deltas是是rpn_bbox_pred预测出来的四个值
+    #boxes为anchor信息，deltas为'rpn_bbox_pred'层信息
+    #boxes.shape[0]=K*A=Height*Width*A
     if boxes.shape[0] == 0:
         return np.zeros((0, deltas.shape[1]), dtype=deltas.dtype)
 
     boxes = boxes.astype(deltas.dtype, copy=False)
-
+    
+    #将anchor的两点坐标格式转换为宽高中心点格式
+    #得到Height*Width*A个anchor的宽，高，中心点的x，y坐标
     widths = boxes[:, 2] - boxes[:, 0] + 1.0
     heights = boxes[:, 3] - boxes[:, 1] + 1.0
     ctr_x = boxes[:, 0] + 0.5 * widths
     ctr_y = boxes[:, 1] + 0.5 * heights
-
+    
+    #从0开始每隔4个值找一个值
+    #deltas本来就只有4列，依次存（dx,dy,dw,dh）,每一行表示一个anchor
+    #0::4表示先取第一个元素，以后每4个取一个，所以取的index为（0,4,8,12,16...），但是deltas本来就只有4列，所以只能取到一个值
     dx = deltas[:, 0::4]
     dy = deltas[:, 1::4]
     dw = deltas[:, 2::4]
     dh = deltas[:, 3::4]
 
+    #预测后的中心点，与w与h
     pred_ctr_x = dx * widths[:, np.newaxis] + ctr_x[:, np.newaxis]
     pred_ctr_y = dy * heights[:, np.newaxis] + ctr_y[:, np.newaxis]
     pred_w = np.exp(dw) * widths[:, np.newaxis]
     pred_h = np.exp(dh) * heights[:, np.newaxis]
 
+    #预测后的（x1,y1,x2,y2）存入 pred_boxes
     pred_boxes = np.zeros(deltas.shape, dtype=deltas.dtype)
     # x1
     pred_boxes[:, 0::4] = pred_ctr_x - 0.5 * pred_w
@@ -62,6 +75,7 @@ def bbox_transform_inv(boxes, deltas):
 
 def clip_boxes(boxes, im_shape):
     """
+    #函数作用：使得boxes位于图片内
     Clip boxes to image boundaries.
     """
 
